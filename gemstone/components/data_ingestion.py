783ceb2eb73ed27c8f0ca2d3ff032bdf1b504e2e
Harshal Kumre <kumreharshalkumar@gmail.com>
2023-04-01 18:37:24 +0530

    data ingestion file updated

--- gemstone/components/data_ingestion.py
+++ gemstone/components/data_ingestion.py
@@ -7,6 +7,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from gemstone.components.data_transformation import DataTransformationConfig
+from gemstone.components.data_transformation import DataTransformation
+
 # Initialize the data Ingestion Configuration
 @dataclass
 class DataIngestionConfig:
@@ -49,3 +52,6 @@ class DataIngestion:
 if __name__=="__main__":
     obj = DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
