7c7298660d901ed8db8d0a88082c39a12ab8aa2b
Harshal Kumre <kumreharshalkumar@gmail.com>
2023-04-02 00:00:37 +0530

    data transformation file updated

--- gemstone/components/data_transformation.py
+++ gemstone/components/data_transformation.py
@@ -1,36 +1,37 @@
-import os, sys
-from gemstone.exception import CustomException
-from gemstone.logger import logging
-import numpy as np  
-import pandas as pd   
-from sklearn.compose import ColumnTransformer
-from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import OrdinalEncoder, StandardScaler
-from sklearn.pipeline import Pipeline
+import sys
 from dataclasses import dataclass
 
-from gemstone.utils import save_object
+import numpy as np 
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OrdinalEncoder,StandardScaler
 
+from gemstone.exception import CustomException
+from gemstone.logger import logging
+import os
+
+from gemstone.utils import save_object
 
 @dataclass
 class DataTransformationConfig:
     preprocessor_obj_file_path = os.path.join('artifacts','preprocessor.pkl')
-
 
 class DataTransformation:
     def __init__(self):
         self.data_transformation_config = DataTransformationConfig()
 
     def get_data_transformation_object(self):
-        """
+        '''
         This function is responsible for data transformation
-        """
+        '''
         try:
             
-            # Define which column should be oridinal-encoded and which should be scaled
+            # Define which columns should be ordinal-encoded and which should be scaled
             categorical_cols = ['cut', 'color','clarity']
             numerical_cols = ['carat', 'depth','table', 'x', 'y', 'z']
-
+            
             # Define the custom ranking for each ordinal variable
             cut_categories = ['Fair', 'Good', 'Very Good','Premium','Ideal']
             color_categories = ['D', 'E', 'F', 'G', 'H', 'I', 'J']
@@ -64,15 +65,14 @@ class DataTransformation:
             )
 
             return preprocessor
-
+        
         except Exception as e:
             logging.info('Exception occured in Data Transformation Phase')
-            raise CustomException(e, sys)
+            raise CustomException(e,sys)
+        
+    def initate_data_transformation(self,train_path,test_path):
 
-
-    def initiate_data_transformation(self, train_path, test_path):
         try:
-            
             # Reading train and test data
             train_df = pd.read_csv(train_path)
             test_df = pd.read_csv(test_path)
@@ -85,7 +85,7 @@ class DataTransformation:
 
             preprocessing_obj = self.get_data_transformation_object()
 
-            target_column_name = "price"
+            target_column_name = 'price'
             drop_columns = [target_column_name,'id']
 
             input_feature_train_df = train_df.drop(columns=drop_columns,axis=1)
@@ -95,25 +95,27 @@ class DataTransformation:
             target_feature_test_df=test_df[target_column_name]
 
             logging.info("Applying preprocessing object on training and testing datasets.")
-
+            
             input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
             input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
 
             train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
             test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
-
+            
             save_object(
-                file_path=self.data_transformation_config.preprocessor_obj_file_path, 
-                obj=preprocessing_obj
-                )
-            logging.info(f"Preprocessor pickle file saved.")
 
-            return(
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                obj=preprocessing_obj
+
+            )
+            logging.info('Preprocessor pickle file saved')
+
+            return (
                 train_arr,
                 test_arr,
                 self.data_transformation_config.preprocessor_obj_file_path,
             )
-
+        
         except Exception as e:
-            logging.info(f"Exception occured in initiate_data_transformation function.")
-            raise CustomException(e, sys)
+            logging.info('Exception occured in initiate_data_transformation function')
+            raise CustomException(e,sys)
